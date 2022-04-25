020c3e274b404e1f2df1e880c58725bd787436ae
asrofil <riidloa@gmail.com>
2022-04-25 13:09:38 +0700

    my solution

--- module01/reverse.go
+++ module01/reverse.go
@@ -1,5 +1,9 @@
 package module01
 
+import (
+	"strings"
+)
+
 // Reverse will return the provided word in reverse
 // order. Eg:
 //
@@ -7,5 +11,14 @@ package module01
 //   Reverse("alphabet") => "tebahpla"
 //
 func Reverse(word string) string {
-	return ""
+	var sb strings.Builder
+	// some characters (e.g kanji) take more one byte
+	// use rune for versatile solutions
+	wordRunes := []rune(word)
+
+	for i := len(wordRunes) - 1; i >= 0; i-- {
+		sb.WriteRune(wordRunes[i])
+	}
+
+	return sb.String()
 }
--- module01/reverse_test.go
+++ module01/reverse_test.go
@@ -12,7 +12,7 @@ func TestReverse(t *testing.T) {
 	}{
 		{"cat", "tac"},
 		{"alphabet", "tebahpla"},
-		// {"日本語", "語本日"},
+		{"日本語", "語本日"},
 	}
 	for _, tc := range tests {
 		t.Run(fmt.Sprintf("%v", tc.word), func(t *testing.T) {
@@ -20,6 +20,7 @@ func TestReverse(t *testing.T) {
 			if got != tc.want {
 				t.Fatalf("Reverse() = %v; want %v", got, tc.want)
 			}
+
 		})
 	}
 }
